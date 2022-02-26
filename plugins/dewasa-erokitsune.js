5e8c6fc01c599ee15afe036b4915298d8870cfaa
PangeranLord <98150373+PangeranLord@users.noreply.github.com>
2022-02-27 01:03:02 +0700

    Update dewasa-erokitsune.js

--- plugins/dewasa-erokitsune.js
+++ plugins/dewasa-erokitsune.js
@@ -10,5 +10,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
 handler.help = ['erokitsune']
 handler.tags = ['dewasa']
 handler.command = /^(erokitsune)$/i
+handler.premium = true
+handler.limit = true
 
 module.exports = handler
