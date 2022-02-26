2f3872e5aec024b48a38e783a9b690c9dd595a42
PangeranLord <98150373+PangeranLord@users.noreply.github.com>
2022-02-27 01:09:26 +0700

    Update dewasa-futanari.js

--- plugins/dewasa-futanari.js
+++ plugins/dewasa-futanari.js
@@ -10,5 +10,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
 handler.help = ['futanari']
 handler.tags = ['dewasa']
 handler.command = /^(futanari)$/i
+handler.premium = true
+handler.limit = true
 
 module.exports = handler
