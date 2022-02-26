7c8c5322a1b124a5a5414e1027c69d97e6ee7ae3
PangeranLord <98150373+PangeranLord@users.noreply.github.com>
2022-02-27 01:16:21 +0700

    Update dewasa-gasm.js

--- plugins/dewasa-gasm.js
+++ plugins/dewasa-gasm.js
@@ -10,5 +10,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
 handler.help = ['gasm']
 handler.tags = ['dewasa']
 handler.command = /^(gasm)$/i
+handler.premium = true
+handler.limit = true
 
 module.exports = handler
