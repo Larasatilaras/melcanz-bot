f4424b61ab7857e6dca869b3103ae2afd359128c
PangeranLord <98150373+PangeranLord@users.noreply.github.com>
2022-02-27 01:17:11 +0700

    Update dewasa-girlsolo.js

--- plugins/dewasa-girlsolo.js
+++ plugins/dewasa-girlsolo.js
@@ -10,5 +10,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
 handler.help = ['girlsolo']
 handler.tags = ['dewasa']
 handler.command = /^(girlsolo)$/i
+handler.premium = true
+handler.limit = true
 
 module.exports = handler
