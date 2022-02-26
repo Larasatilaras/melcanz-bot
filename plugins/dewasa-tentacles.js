a323abd72442de2d9ddd9576d51df90ad76b6c1d
PangeranLord <98150373+PangeranLord@users.noreply.github.com>
2022-02-27 01:48:46 +0700

    Update dewasa-tentacles.js

--- plugins/dewasa-tentacles.js
+++ plugins/dewasa-tentacles.js
@@ -10,5 +10,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
 handler.help = ['tentacles']
 handler.tags = ['dewasa']
 handler.command = /^(tentacles)$/i
+handler.premium = true
+handler.limit = true
 
 module.exports = handler
