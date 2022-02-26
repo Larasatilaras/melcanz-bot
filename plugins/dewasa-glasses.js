af269c657a6f47a9262b6d4d2fbd6e565ae85d0a
PangeranLord <98150373+PangeranLord@users.noreply.github.com>
2022-02-27 01:20:03 +0700

    Update dewasa-glasses.js

--- plugins/dewasa-glasses.js
+++ plugins/dewasa-glasses.js
@@ -10,5 +10,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
 handler.help = ['glasses']
 handler.tags = ['dewasa']
 handler.command = /^(glasses)$/i
+handler.premium = true
+handler.limit = true
 
 module.exports = handler
