68f0e99cbe35e247658ade9dd53ce5977716c456
PangeranLord <98150373+PangeranLord@users.noreply.github.com>
2022-02-27 01:01:24 +0700

    Update dewasa-erofeet.js

--- plugins/dewasa-erofeet.js
+++ plugins/dewasa-erofeet.js
@@ -10,5 +10,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
 handler.help = ['erofeet']
 handler.tags = ['dewasa']
 handler.command = /^(erofeet)$/i
+handler.premium = true
+handler.limit = true
 
 module.exports = handler
