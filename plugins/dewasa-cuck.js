cd6e98aeab1ac07233b2733db2d68d1bc8a6709e
PangeranLord <98150373+PangeranLord@users.noreply.github.com>
2022-02-27 00:53:54 +0700

    Update dewasa-cuck.js

--- plugins/dewasa-cuck.js
+++ plugins/dewasa-cuck.js
@@ -5,10 +5,12 @@ let handler = async (m, { conn, usedPrefix, command }) => {
     if (!res.ok) throw eror
     let json = await res.json()
     if (!json.status) throw json
-    conn.sendButtonImg(m.chat, json.url, 'Tobat banh', wm, 'lagi', `${usedPrefix + command}`, m)
+    conn.sendButtonImg(m.chat, json.url, '_*Tobat Om/Tante:v*_', wm, 'lagi', `${usedPrefix + command}`, m)
 }
 handler.help = ['cuckold']
 handler.tags = ['dewasa']
 handler.command = /^(cuckold)$/i
+handler.premium = true
+handler.limit = true
 
 module.exports = handler
