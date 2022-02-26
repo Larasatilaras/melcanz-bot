8fd4a33222991cb89c9b40dabe46fab26e43fae6
PangeranLord <98150373+PangeranLord@users.noreply.github.com>
2022-02-27 01:30:26 +0700

    Update dewasa-kemonomimi.js

--- plugins/dewasa-kemonomimi.js
+++ plugins/dewasa-kemonomimi.js
@@ -10,5 +10,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
 handler.help = ['kemonomimi']
 handler.tags = ['dewasa']
 handler.command = /^(kemonomimi)$/i
+handler.premium = true
+handler.limit = true
 
 module.exports = handler
