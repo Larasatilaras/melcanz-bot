a646c9e4d2851edb850a9d28789c332f560ac9a4
PangeranLord <98150373+PangeranLord@users.noreply.github.com>
2022-02-27 01:18:52 +0700

    Update dewasa-girlsologif.js

--- plugins/dewasa-girlsologif.js
+++ plugins/dewasa-girlsologif.js
@@ -10,5 +10,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
 handler.help = ['girlsologif']
 handler.tags = ['dewasa']
 handler.command = /^(girlsologif)$/i
+handler.premium = true
+handler.limit = true
 
 module.exports = handler
