2238ec98c3b88baf8decaed79971ae07df932089
PangeranLord <98150373+PangeranLord@users.noreply.github.com>
2022-02-27 01:25:45 +0700

    Update dewasa-highs.js

--- plugins/dewasa-highs.js
+++ plugins/dewasa-highs.js
@@ -10,5 +10,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
 handler.help = ['thighs']
 handler.tags = ['dewasa']
 handler.command = /^(thighs)$/i
+handler.premium = true
+handler.limit = true
 
 module.exports = handler
