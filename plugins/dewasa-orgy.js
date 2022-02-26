40242e0a702782896a612476c0b146080fc57066
PangeranLord <98150373+PangeranLord@users.noreply.github.com>
2022-02-27 01:41:21 +0700

    Update dewasa-orgy.js

--- plugins/dewasa-orgy.js
+++ plugins/dewasa-orgy.js
@@ -10,5 +10,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
 handler.help = ['orgy']
 handler.tags = ['dewasa']
 handler.command = /^(orgy)$/i
+handler.premium = true
+handler.limit = true
 
 module.exports = handler
