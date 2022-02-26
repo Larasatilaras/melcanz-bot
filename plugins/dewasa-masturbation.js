b9ea225bd4c111d8f21302763a0d0cad367514d9
PangeranLord <98150373+PangeranLord@users.noreply.github.com>
2022-02-27 01:39:30 +0700

    Update dewasa-masturbation.js

--- plugins/dewasa-masturbation.js
+++ plugins/dewasa-masturbation.js
@@ -10,5 +10,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
 handler.help = ['masturbation']
 handler.tags = ['dewasa']
 handler.command = /^(masturbation)$/i
+handler.premium = true
+handler.limit = true
 
 module.exports = handler
