e2c2f55aed7b17785530f540f91808c8d1147611
PangeranLord <98150373+PangeranLord@users.noreply.github.com>
2022-02-27 01:43:46 +0700

    Update dewasa-pussy.js

--- plugins/dewasa-pussy.js
+++ plugins/dewasa-pussy.js
@@ -10,5 +10,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
 handler.help = ['pussy']
 handler.tags = ['dewasa']
 handler.command = /^(pussy)$/i
+handler.premium = true
+handler.limit = true
 
 module.exports = handler
