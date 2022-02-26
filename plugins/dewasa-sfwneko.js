454116630039675e3f904e8c9d65bd806455e9dc
PangeranLord <98150373+PangeranLord@users.noreply.github.com>
2022-02-27 01:45:58 +0700

    Update dewasa-sfwneko.js

--- plugins/dewasa-sfwneko.js
+++ plugins/dewasa-sfwneko.js
@@ -10,5 +10,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
 handler.help = ['sfwneko']
 handler.tags = ['dewasa']
 handler.command = /^(sfwneko)$/i
+handler.premium = true
+handler.limit = true
 
 module.exports = handler
