6078bbd4eadcb474b908329ea359188b963dc4cf
PangeranLord <98150373+PangeranLord@users.noreply.github.com>
2022-02-27 01:40:29 +0700

    Update dewasa-neko.js

--- plugins/dewasa-neko.js
+++ plugins/dewasa-neko.js
@@ -10,5 +10,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
 handler.help = ['neko']
 handler.tags = ['dewasa']
 handler.command = /^(neko)$/i
+handler.premium = true
+handler.limit = true
 
 module.exports = handler
