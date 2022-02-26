9f270f68edf3b40464def50193dc698c83d1442d
PangeranLord <98150373+PangeranLord@users.noreply.github.com>
2022-02-27 01:47:23 +0700

    Update dewasa-spank.js

--- plugins/dewasa-spank.js
+++ plugins/dewasa-spank.js
@@ -10,5 +10,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
 handler.help = ['spank']
 handler.tags = ['dewasa']
 handler.command = /^(spank)$/i
+handler.premium = true
+handler.limit = true
 
 module.exports = handler
