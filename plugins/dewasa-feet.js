440353a152a66bea2931ddbb002f2e1588369aaf
PangeranLord <98150373+PangeranLord@users.noreply.github.com>
2022-02-27 01:06:04 +0700

    Update dewasa-feet.js

--- plugins/dewasa-feet.js
+++ plugins/dewasa-feet.js
@@ -10,5 +10,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
 handler.help = ['feet']
 handler.tags = ['dewasa']
 handler.command = /^(feet)$/i
+handler.premium = true
+handler.limit = true
 
 module.exports = handler
