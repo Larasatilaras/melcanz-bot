83e28b9752c4f74e116b27c0ddfd9a058d13e065
PangeranLord <98150373+PangeranLord@users.noreply.github.com>
2022-02-27 00:41:06 +0700

    Update dewasa-anal.js

--- plugins/dewasa-anal.js
+++ plugins/dewasa-anal.js
@@ -10,5 +10,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
 handler.help = ['anal']
 handler.tags = ['dewasa']
 handler.command = /^(anal)$/i
+handler.premium = true
+handler.limit = true
 
 module.exports = handler
